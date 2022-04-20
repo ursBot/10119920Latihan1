79993a277a0e75797f537b5402067320e6b26cda
ursBot <umar.10119920@mahasiswa.unikom.ac.id>
2022-04-20 11:22:53 +0700

    first commit

---
+++ app/src/main/java/com/apps/umar10119920latihan1/MainActivity.java
@@ -0,0 +1,20 @@
+package com.apps.umar10119920latihan1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * NAMA     : UMAR SAID ADI PRANOTO
+ * NIM      : 10119920
+ * KELAS    : IF10
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
